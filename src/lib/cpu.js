24e52ac68443df73907b96faf31b2412a3f16bd7
gitcyco <gitcyco@users.noreply.github.com>
2023-12-04 14:33:44 -0800

    flesh out memory and instruction loading

--- src/lib/cpu.js
+++ src/lib/cpu.js
@@ -4,22 +4,60 @@ const SOUND_TIMER_INTERVAL = 15;
 // Some programs will expect that the first byte of the loaded program
 // begin at address 0x200 (512) due to historical reasons.
 // Changing this could break things, so we will just load the font data into low memory.
-const LOAD_ADDRESS = 0x200;
+const LOAD_ADDRESS_BYTE = 0x200;
+const LOAD_ADDRESS_WORD = 0x100;
 
 class CPU {
   constructor(display, keyboard, ramsize, stacksize) {
     console.log("new cpu");
-    this._ram = new ArrayBuffer(ramsize);
-    this.wordInterface = new Int16Array(this._ram);
-    this.byteInterface = new Int8Array(this._ram);
-    this.stack = new Stack(this._ram, stacksize);
-    this.registers = new Array(16).fill(0);
-    this._delayTimer = 0;
-    this._soundtimer = 0;
-    this._indexReg = 0;
-    this._programCounter = 0;
-    this._dtClearInterval = null;
-    this._stClearInterval = null;
+    this.display = display;
+    this.keyboard = keyboard;
+    this._ramsize = ramsize;
+    this._stacksize = stacksize;
+    this.initialize();
+    // this._ram = new ArrayBuffer(this._ramsize);
+    // this.wordInterface = new Uint16Array(this._ram);
+    // this.byteInterface = new Uint8Array(this._ram);
+    // this.stack = new Stack(this._ram, this._stacksize);
+    // this.registers = new Array(16).fill(0);
+    // this._delayTimer = 0;
+    // this._soundtimer = 0;
+    // this._indexReg = 0;
+    // this._programCounter = LOAD_ADDRESS_BYTE;
+    // this._dtClearInterval = null;
+    // this._stClearInterval = null;
+    // this._loadFileFinished = false;
+  }
+  fetchInstruction() {
+    if (!this._loadFileFinished) {
+      console.log("No program has been loaded.");
+      return null;
+    }
+    if (this._programCounter >= this.byteInterface.length - 2) {
+      throw new RangeError("Program Counter has exceeded memory bounds.");
+    }
+    const byte1 = this.byteInterface[this._programCounter];
+    const byte2 = this.byteInterface[this._programCounter + 1];
+    this._programCounter += 2;
+    const instructionWord = (byte1.toString(16).padStart(2, 0) + byte2.toString(16).padStart(2, 0)).toUpperCase();
+    console.log("instruction:", instructionWord);
+  }
+  async loadFromLocalFile(rom) {
+    console.log("LOADING:", rom);
+    const buffer = await rom.arrayBuffer();
+    const bytes = new Uint8Array(buffer);
+    const words = new Uint16Array(buffer);
+    console.log("raw rom:", bytes, words);
+    for (let i = 0; i < bytes.length; i++) {
+      this.byteInterface[LOAD_ADDRESS_BYTE + i] = bytes[i];
+    }
+    // for (let i = 0; i < this.byteInterface.length; i += 2) {
+    //   // First 4 bits: 61440
+    //   // Last 12 bits: 4095
+    //   const view1 = new DataView(this._ram);
+    //   console.log("index:", i, view1.getUint16(i).toString(16).padStart(4, 0));
+    // }
+    this._loadFileFinished = true;
   }
   set delayTimer(val) {
     if (this._dtClearInterval) clearInterval(this._dtClearInterval);
@@ -54,6 +92,22 @@ class CPU {
   }
   get soundTimer() {
     return this._soundTimer;
+  }
+  initialize() {
+    this._ram = new ArrayBuffer(this._ramsize);
+    this.wordInterface = new Uint16Array(this._ram);
+    this.byteInterface = new Uint8Array(this._ram);
+    this.stack = new Stack(this._ram, this._stacksize);
+    this.registers = new Array(16).fill(0);
+    this._delayTimer = 0;
+    this._soundtimer = 0;
+    this._indexReg = 0;
+    this._programCounter = LOAD_ADDRESS_BYTE;
+    if (this._dtClearInterval) clearInterval(this._dtClearInterval);
+    if (this._stClearInterval) clearInterval(this._stClearInterval);
+    this._dtClearInterval = null;
+    this._stClearInterval = null;
+    this._loadFileFinished = false;
   }
 }
 
--- src/webapp.js
+++ src/webapp.js
@@ -6,10 +6,31 @@ const display = new BrowserDisplay(64, 32, 10, 10);
 const keyboard = new BrowserKeyboard();
 const cpu = new CPU(display, keyboard, 4096, 48);
 
+// cpu.loadFromLocalFile("../roms/IBM Logo.ch8");
+
+const loadButton = document.getElementById("load-rom");
+loadButton.addEventListener("click", async (e) => {
+  const rom = document.getElementById("input-rom").files[0];
+  if (rom) {
+    console.log("loaded rom:", rom);
+    await cpu.loadFromLocalFile(rom);
+  }
+});
+
+const stepButton = document.getElementById("step-rom");
+stepButton.addEventListener("click", (e) => {
+  cpu.fetchInstruction();
+});
+
+const resetButton = document.getElementById("reset-cpu");
+resetButton.addEventListener("click", (e) => {
+  cpu.initialize();
+});
+
 // randomize();
 
-display.setPixelsByte(22, 10, 202);
-display.setPixelsByte(22, 15, 212);
+// display.setPixelsByte(22, 10, 202);
+// display.setPixelsByte(22, 15, 212);
 display.setPixelsByte(22, 20, 242);
 display.paint();
 
